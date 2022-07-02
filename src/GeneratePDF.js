3ed46933da2e00f12b5829d3f28bb0fc13d5df6b
shijinbennyabraham <shijinptpm77@gmail.com>
2022-07-02 12:15:23 +0530

    changes

--- src/GeneratePDF.js
+++ src/GeneratePDF.js
@@ -41,7 +41,7 @@ function GeneratePDF({ setformno, scrollTop, user, details, setDetails }) {
             {/* <h1 className="text-4xl text-tertiary font-light mt-8 text-center">Application for Hostel Admission</h1> */}
             <h2 className="text-center mt-1 font-bold text-gray-500">Review your application</h2>
             <div className="w-11/12 md:w-9/12 mx-auto my-8 bg-white shadow-md rounded-xl pb-2">
-                <PDFExport paperSize="A4" scale={0.6} margin="1.3cm" ref={pdfExportComponent}>
+               {applicationNo!=-1&&( <PDFExport paperSize="A4" scale={0.6} margin="1.3cm" ref={pdfExportComponent}>
                     <dl className="review-section">
                         <h2>Application CET/Hostel/{applicationNo}</h2>
                         <h3 className="text-3xl text-secondary">Personal Details</h3>
@@ -126,7 +126,7 @@ function GeneratePDF({ setformno, scrollTop, user, details, setDetails }) {
                   
          
                     
-                </PDFExport>
+                </PDFExport>)}
             </div>
             <div className="flex justify-center space-x-6 mb-10">
                 <button
