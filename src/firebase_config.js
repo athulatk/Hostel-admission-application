e30112d6ef8dab100a33fb0fe250276c76a857b3
shijinbennyabraham <shijinptpm77@gmail.com>
2022-07-03 11:49:31 +0530

    sign up with admission number

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -13,22 +13,22 @@ const firebaseConfig = {
     // appId: process.env.REACT_APP_APP_ID,
 
     //official
-    // apiKey: "AIzaSyDQX7vI92RCqlKK1Wy11CrDjfQ3BOLMvCQ",
-    // authDomain: "cet-hostel-application-fe7bd.firebaseapp.com",
-    // databaseURL: "https://cet-hostel-application-fe7bd-default-rtdb.firebaseio.com",
-    // projectId: "cet-hostel-application-fe7bd",
-    // storageBucket: "cet-hostel-application-fe7bd.appspot.com",
-    // messagingSenderId: "1026520077764",
-    // appId: "1:1026520077764:web:9c919788db3450f2de4091"
+    apiKey: "AIzaSyDQX7vI92RCqlKK1Wy11CrDjfQ3BOLMvCQ",
+    authDomain: "cet-hostel-application-fe7bd.firebaseapp.com",
+    databaseURL: "https://cet-hostel-application-fe7bd-default-rtdb.firebaseio.com",
+    projectId: "cet-hostel-application-fe7bd",
+    storageBucket: "cet-hostel-application-fe7bd.appspot.com",
+    messagingSenderId: "1026520077764",
+    appId: "1:1026520077764:web:9c919788db3450f2de4091"
 
     //test
-    apiKey: "AIzaSyCuiZO5iVecmwx399NVctqIPnFqTX8KfD8",
-    authDomain: "cet-hostel-application.firebaseapp.com",
-    databaseURL: "https://cet-hostel-application-default-rtdb.firebaseio.com",
-    projectId: "cet-hostel-application",
-    storageBucket: "cet-hostel-application.appspot.com",
-    messagingSenderId: "222559251801",
-    appId: "1:222559251801:web:72e43d51793eeb3276b048"
+    // apiKey: "AIzaSyCuiZO5iVecmwx399NVctqIPnFqTX8KfD8",
+    // authDomain: "cet-hostel-application.firebaseapp.com",
+    // databaseURL: "https://cet-hostel-application-default-rtdb.firebaseio.com",
+    // projectId: "cet-hostel-application",
+    // storageBucket: "cet-hostel-application.appspot.com",
+    // messagingSenderId: "222559251801",
+    // appId: "1:222559251801:web:72e43d51793eeb3276b048"
 };
 
 export const app = initializeApp(firebaseConfig);
